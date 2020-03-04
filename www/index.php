e33c7dfabe88425a45497228ba630a58ba93b5f8
d.berezin@aventon.ru <basone329>
2020-03-04 21:24:45 +0300

    первый тест на php

---
+++ www/index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+    <title>Минисайт</title>
+</head>
+<body>
+
+<section>
+    <p>Много-много полезной информации</p>
+</section>
+<?php include "footer.php"; ?>
+</body>
+</html>
